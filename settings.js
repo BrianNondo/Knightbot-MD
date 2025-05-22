928454fbe8afa67b0bd63787f03cbee4611e70a2
BrianNondo <143845074+BrianNondo@users.noreply.github.com>
2025-05-22 18:58:52 +0200

    Update settings.js
    cn

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Pluton Bot",
   botOwner: 'brian', // Your name
-  ownerNumber: '260967513164', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '260978196887', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
